fb0bbf5cee622fdcc6d6b6115289ca6664fd3922
Lucy Fox <lucyreneefox@gmail.com>
2016-06-07 15:39:20 -0600

    add sock validations sock validations for presence and the unquiness of name on a sock entity.

--- app/models/sock.rb
+++ app/models/sock.rb
@@ -4,4 +4,9 @@ class Sock < ActiveRecord::Base
   belongs_to :size
 
   validates :name, presence: true, uniqueness: true
+  validates :foot, presence: true
+  validates :price, presence: true
+  validates :style_id, presence: true
+  validates :category_id, presence: true
+  validates :size_id, presence: true
 end
--- spec/models/sock_spec.rb
+++ spec/models/sock_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 RSpec.describe Sock, type: :model do
   context "validations, presence" do
     it { should validate_presence_of(:name) }
-
+    it { should validate_presence_of(:foot)}
+    it { should validate_presence_of(:price)}
+    it { should validate_presence_of(:style_id)}
+    it { should validate_presence_of(:category_id)}
+    it { should validate_presence_of(:size_id)}
 
   end
+
   context "validations, uniqueness" do
     it { should validate_uniqueness_of(:name) }
   end
