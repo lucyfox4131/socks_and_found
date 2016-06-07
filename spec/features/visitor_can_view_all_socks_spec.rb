fffd1995eda60acd62c31a0d5c49dcd00b8091d3
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-06-07 01:51:40 -0600

    Added sock image URL; tests pass

--- spec/features/visitor_can_view_all_socks_spec.rb
+++ spec/features/visitor_can_view_all_socks_spec.rb
@@ -8,10 +8,8 @@ RSpec.feature "Visitor views all socks" do
     within("#all_socks") do
       expect(page).to have_content sock_1.name
       expect(page).to have_content sock_1.price
-      expect(page).to have_content sock_2.name
-      expect(page).to have_content sock_2.price
-      expect(page).to have_content sock_3.name
-      expect(page).to have_content sock_3.price
+      expect(page).to have_content sock_2.foot
+      expect(page).to have_content sock_3.image_url
     end
   end
 end
