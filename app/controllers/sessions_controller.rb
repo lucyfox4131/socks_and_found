02489e3f8328cc5829c87d25807c4b726ab6ef04
Charles Kaminer <ckamine@g.clemson.edu>
2016-06-07 20:31:50 -0600

    add login logout functionality created login logout tests with happy and sad paths tests passing updated sessions controller with destroy method for logout added flash messages to application view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,21 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(username: params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to dashboard_path
+    else
+      flash.now[:error] = "Invalid login. Please try again."
+      render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    flash[:notice] = "Goodbye!"
+    redirect_to login_path
+  end
+
 end
