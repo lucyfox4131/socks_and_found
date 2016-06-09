8d1374c29fd448d2213b81ccf03a24f3f6d29cfe
Charles Kaminer <ckamine@g.clemson.edu>
2016-06-08 22:26:40 -0600

    fix password confirmation password confirmation added to user_params in user controller password confirmations working properly for account creation need to fix failed login redirection route test written

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true
 
+
 end
--- spec/features/visitor_can_create_account_and_get_to_their_dashboard_spec.rb
+++ spec/features/visitor_can_create_account_and_get_to_their_dashboard_spec.rb
@@ -24,4 +24,22 @@ RSpec.feature "visitor creates an account" do
       expect(page).to have_content("Logout")
     end
   end
+
+  context "password confirmation does not match password" do
+    scenario "receive flash about not matching and brought back to new user form" do
+      visit login_path
+      click_link "Create Account"
+
+      fill_in "Name", with: "Bob"
+      fill_in "Username", with: "Username"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "Password"
+      click_on "Create Account"
+
+      expect(current_path).to eq(new_user_path)
+
+      expect(page).to_not have_content("Password confirmation doesn't match Passowrd")
+      expect(page).to have_content("Logout")
+    end
+  end
 end
