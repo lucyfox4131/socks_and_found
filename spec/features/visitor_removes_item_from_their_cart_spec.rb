969b92bd149a84e2a54b80c27faadd206e9981ac
Lucy Fox <lucyreneefox@gmail.com>
2016-06-09 15:39:42 -0600

    add order security only a current and logged in user is able to access the orders path. Any other user will be redirected to the login path if they try to view orders.

--- spec/features/visitor_removes_item_from_their_cart_spec.rb
+++ spec/features/visitor_removes_item_from_their_cart_spec.rb
@@ -21,11 +21,3 @@ RSpec.feature "visitor removes sock from cart" do
     end
   end
 end
-
-# Background: My cart has an item in it
-
-# Then my current page should be "/cart"
-# And I should see a message styled in green
-# And the message should say "Successfully removed SOME_ITEM from your cart."
-# And the title "SOME_ITEM" should be a link to that item in case the user wants to add it back
-# And I should not see the item listed in cart
