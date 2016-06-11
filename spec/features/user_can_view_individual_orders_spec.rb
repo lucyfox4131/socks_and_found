64dd0d8e908802f70bf9164bd67f7d4d9b23c55c
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-06-11 14:12:18 -0600

    Fixing hound complaints

--- spec/features/user_can_view_individual_orders_spec.rb
+++ spec/features/user_can_view_individual_orders_spec.rb
@@ -4,13 +4,10 @@ RSpec.feature "an existing user can view their individual past orders" do
   scenario "they are redirected to order path" do
     user = create(:user)
     order_1 = Order.create(user_id: user.id)
-    order_2 = Order.create(user_id: user.id)
-    order_3 = Order.create(user_id: user.id)
-
     login(user)
     visit "/orders"
 
-    click_link "#{order_1.id}"
+    click_link order_1.id.to_s
 
     expect(current_path).to eq order_path(order_1)
     expect(page).to have_content(order_1.id)
@@ -29,13 +26,12 @@ RSpec.feature "an existing user can view their individual past orders" do
                                               sock_price: sock_2.price)
     total_price = 2 * order_item_1.sock_price + 5 * order_item_2.sock_price
     total_quantity = order_item_1.quantity + order_item_2.quantity
-    # add order-items to order once order-items are implemented
     visit order_path(order_1)
     within "#order_item" do
       expect(page).to have_content(
-                  "$#{order_item_1.quantity * order_item_1.sock_price}")
+                    "$#{order_item_1.quantity * order_item_1.sock_price}")
       expect(page).to have_content(
-                  "$#{order_item_2.quantity * order_item_2.sock_price}")
+                    "$#{order_item_2.quantity * order_item_2.sock_price}")
       expect(page).to have_link(sock_1.name)
     end
 
@@ -53,25 +49,7 @@ RSpec.feature "an existing user can view their individual past orders" do
     within "#order_details" do
       expect(page).to have_content("completed")
       expect(page).to have_content(
-                  order_1.updated_at.strftime("%A, %B %d, %Y at %I:%M%p"))
+                    order_1.updated_at.strftime("%A, %B %d, %Y at %I:%M%p"))
     end
   end
 end
-
-
-
-# Background: An existing user that has one previous order
-#
-# As an authenticated user
-# When I visit "/orders"
-# Then I should see my past order
-# And I should see a link to view that order
-# And when I click that link
-# Then I should see each item that was order with the quantity and line-item subtotals
-# And I should see links to each item's show page
-# And I should see the current status of the order (ordered, paid, cancelled, completed)
-# And I should see the total price for the order
-# And I should see the date/time that the order was submitted
-#
-# If the order was completed or cancelled
-# Then I should see a timestamp when the action took place
