2f6f61a5a681bcc755e7322e2c12ee73003d4111
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-06-11 12:10:32 -0600

    View individual order information on that order's show page Add migration to add status column Update order show page to show attributes of individual order Add total methods to find total quantity and total price in order model

--- spec/features/user_can_view_individual_orders_spec.rb
+++ spec/features/user_can_view_individual_orders_spec.rb
@@ -18,13 +18,43 @@ RSpec.feature "an existing user can view their individual past orders" do
 
   scenario "they see order item links, prices, statuses, date/time" do
     user = create(:user)
-    order_1 = Order.create(user_id: user.id)
+    login(user)
+    order_1 = Order.create(user_id: user.id, status: "ordered")
+    sock_1, sock_2 = create_list(:sock, 2)
+    order_item_1 = order_1.order_items.create(sock_id: sock_1.id,
+                                              quantity: 2,
+                                              sock_price: sock_1.price)
+    order_item_2 = order_1.order_items.create(sock_id: sock_2.id,
+                                              quantity: 5,
+                                              sock_price: sock_2.price)
+    total_price = 2 * order_item_1.sock_price + 5 * order_item_2.sock_price
+    total_quantity = order_item_1.quantity + order_item_2.quantity
     # add order-items to order once order-items are implemented
-
     visit order_path(order_1)
+    within "#order_item" do
+      expect(page).to have_content(
+                  "$#{order_item_1.quantity * order_item_1.sock_price}")
+      expect(page).to have_content(
+                  "$#{order_item_2.quantity * order_item_2.sock_price}")
+      expect(page).to have_link(sock_1.name)
+    end
 
-    # add assertions upon creation of order-item, status, and checkout functionality
-    # don't forget to delete user story below
+    within "#order_details" do
+      expect(page).to have_content("ordered")
+      expect(page).to have_content(total_price)
+      expect(page).to have_content(
+                      order_1.created_at.strftime("%A, %B %d, %Y at %I:%M%p"))
+      expect(page).to have_content(total_quantity)
+      expect(page).to_not have_content("Updated")
+    end
+
+    order_1.update_attributes(status: "completed")
+    visit order_path(order_1)
+    within "#order_details" do
+      expect(page).to have_content("completed")
+      expect(page).to have_content(
+                  order_1.updated_at.strftime("%A, %B %d, %Y at %I:%M%p"))
+    end
   end
 end
 
