4782308d7152d0d1e0f417a8aac4045f32dbb411
Charles Kaminer <ckamine@g.clemson.edu>
2016-06-14 16:42:05 -0600

    set up tweet functionality change configs to not have scocks update consumer keys for prod and dev reconfigure development.rb to read from ENV reconfigure production.rb to read from ENV able to send tweets from app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,4 +46,7 @@ Rails.application.configure do
     }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.twitter_key = ENV["TWITTER_CONSUMER_KEY"]
+  config.twitter_secret = ENV["TWITTER_CONSUMER_SECRET"]
 end
