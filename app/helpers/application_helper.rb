bf355b3dc68c913c01e82bb780049e774b4763b8
Lucy Fox <lucyreneefox@gmail.com>
2016-06-15 13:10:34 -0600

    refactored application layout file Application layout logic refactored into a helper method within the application helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,23 @@ module ApplicationHelper
     presenter
   end
 
-  
+  def user_button_display
+    button_array = []
+    if current_user
+      button_array << link_to("Logout", logout_path, method: :delete)
+      button_array << current_admin_display
+    else
+      button_array << link_to("Login", login_path)
+      button_array << link_to("Login with Twitter", twitter_login_path)
+    end
+    return button_array
+  end
+
+  def current_admin_display
+    if current_admin?
+      link_to("Logged in as #{current_user.name}", admin_dashboard_path)
+    else
+      link_to("Logged in as #{current_user.name}", dashboard_path)
+    end
+  end
 end
