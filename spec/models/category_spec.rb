99f11a06a8adb12825e035ed641edb6b6d3c7380
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-06-15 10:41:37 -0600

    Closes #103 closes #85 add sad path testing to model tests Add sad path testing for Category Add sad path testing for Size Add sad path testing for Style Add sad path and method testing in cart, order, sock

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe Category, type: :model do
     it { should validate_presence_of(:title) }
     it { should validate_uniqueness_of(:title) }
   end
+
+  context "category not valid without validations" do
+    category = Category.new(title: "")
+    it "is not valid" do
+      expect(category).to_not be_valid
+    end
+  end
+
+  context "category not valid if not unique" do
+    category = Category.create(title: "test")
+    category2 = Style.new(name: category.title)
+    it "is not valid" do
+      expect(category2).to_not be_valid
+    end
+  end
 end
